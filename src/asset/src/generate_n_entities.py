eb63bc482a8baa899390f0784861b5a2df401ccc
nsensfel <SpamShield0@noot-noot.org>
2018-01-08 16:13:38 +0100

    Fixes CSS colored classes generation script.

--- src/asset/src/generate_n_entities.py
+++ src/asset/src/generate_n_entities.py
@@ -15,14 +15,14 @@ with open(CSS_TEMPLATE, 'r') as template_file:
     template=template_file.read()
 
 for i in range(N):
-    colors = sys.stdin.readline().split(' ')
+    colors = sys.stdin.readline().strip()
     print(
         template.replace(
             '$ID$',
             str(i)
         ).replace(
             '$COLOR$',
-            sys.stdin.readline()
+            colors
         )
     )
 
